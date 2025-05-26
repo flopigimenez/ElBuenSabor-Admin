03318c5d8d1402cdad55211a2bf03181eeddeaf0
flopigimenez <fgimenezmorcos@gmail.com>
2025-05-26 19:26:32 -0300

    proyecto creado

---
+++ src/routes/RoutesApp.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+const RoutesApp = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RoutesApp
